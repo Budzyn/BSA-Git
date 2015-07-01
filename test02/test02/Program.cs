2dda1f69440c25a421b6a8df35d0ce7b79876dee
Jaroslav <black_witcher@ukr.net>
2015-07-02 00:46:14 +0300

    4444: add new Program.cs

---
+++ test02/test02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace test02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+            Console.Write(date);
+            Console.ReadKey();
+
+        }
+    }
+}
