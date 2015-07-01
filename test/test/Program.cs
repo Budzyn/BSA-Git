54c40bf0db011f7d2cd6e4a79e92f5bae57b08f7
Jaroslav <black_witcher@ukr.net>
2015-07-02 00:35:34 +0300

    2222: add new local variable

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -10,9 +10,13 @@ namespace test
         static void Main(string[] args)
         {
             string mystr;
+            int i;
             Console.Write("Enter your name: ");
             mystr = Convert.ToString(Console.ReadLine());
+            Console.Write("How old are you? ");
+            i = Convert.ToInt32(Console.ReadLine());
             Console.ReadKey();
+           
         }
     }
 }
