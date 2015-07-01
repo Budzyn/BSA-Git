1430c9cc635f0a9bb917e6d5470aa03aabe04d30
Jaroslav <black_witcher@ukr.net>
2015-07-02 00:12:32 +0300

    1111: add new test.csproj

---
+++ test/test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystr;
+            Console.Write("Enter your name: ");
+            mystr = Convert.ToString(Console.ReadLine());
+            Console.ReadKey();
+        }
+    }
+}
