6d8e4f77c4b88f41da583b25ee823827730476c5
Jaroslav <black_witcher@ukr.net>
2015-07-07 02:23:30 +0300

    1255: add linq

---
+++ MyLINQ/MyLINQ/MyLINQ/Models/Ask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyLINQ.Models
+{
+    class Ask
+    {
+        public string Category { get; set; }
+        public string ListAsk { get; set; }
+    }
+}
---
+++ MyLINQ/MyLINQ/MyLINQ/Models/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyLINQ.Models
+{
+    class Test
+    {
+        public string Name { get; set; }
+        public List <Ask> Category { get; set; }
+        public List <Ask> ListAsk { get; set; }
+        public int TimeMax { get; set; }
+        public int MarkPass { get; set; }
+    }
+}
